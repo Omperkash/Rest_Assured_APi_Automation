99eed1a5226be1a1dfcad54fc2ad059530a74023
Omperkash <36916194+Omperkash@users.noreply.github.com>
2022-02-20 17:58:30 +0100

    updated with extent report

--- src/test/java/qatests/Shared.java
+++ src/test/java/qatests/Shared.java
@@ -1,11 +1,10 @@
 package qatests;
 
 import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
+
 
 public class Shared {
 
